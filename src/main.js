58e8ac36d0bcb363227ae194c4029729a68d3654
Kat Law <kathleen.law@multiverse.io>
2023-08-08 12:36:51 -0400

    adding associations

--- src/main.js
+++ src/main.js
@@ -1,48 +1,33 @@
 const { db } = require("../db/connection.js");
-const Pokemon = require("./model");
+const { Pokemon, Trainer, Badge } = require("./models");
 
 async function main() {
-  await db.sync({ force: true });
+  const trainers = await Trainer.findAll();
+  const pokemon = await Pokemon.findAll();
+  const badges = await Badge.findAll();
 
-  //   CREATE
-  const Pikachu = await Pokemon.create({
-    name: "Pikachu",
-    type: "Electric",
-    weight: 6,
-  });
+  console.log(trainers[0]);
 
-  await Pokemon.bulkCreate([
-    {
-      name: "Bulbasaur",
-      type: "Grass/Poison",
-      weight: 6.9,
-    },
-    {
-      name: "Charmander",
-      type: "Fire",
-      weight: 8.5,
-    },
-    {
-      name: "Squirtle",
-      type: "Water",
-      weight: 9,
-    },
-  ]);
+  await trainers[0].addPokemon(pokemon[0]);
+  await trainers[0].addPokemon([pokemon[1], pokemon[2]]);
 
-  //READ
-  const currentPokemon = await Pokemon.findByPk(3);
-  const allPokemon = await Pokemon.findAll();
+  const trainer = await Trainer.findOne({where: {name: "Red"}});
+  console.log(JSON.stringify(trainer));
 
-  // UPDATE
-  const updateResult = await Pokemon.update(
-    { name: "Ivysaur", weight: 13 },
-    { where: { name: "Bulbasaur" } }
-  );
+  const trainerWithPokemon = await Trainer.findOne({
+    where: {
+      name: "Red"
+    }, 
+    include: Pokemon});
+  console.log(JSON.stringify(trainerWithPokemon, null, 2));
 
-  //DELETE
-  const deleteResult = await Pokemon.destroy({ where: { name: "Squirtle" } });
+  await trainers[0].addBadge(badges[0]);
+  await trainers[1].addBadge(badges[0]);
+  await trainers[1].addBadge(badges[1]);
 
-  console.log(JSON.stringify(deleteResult, null, 2));
+  console.log('---Testing Many to Many Eager Loading---')
+  const trainersWithBadges = await Trainer.findAll({include: Badge});
+  console.log(JSON.stringify(trainersWithBadges, null, 2));
 }
 
 main();
